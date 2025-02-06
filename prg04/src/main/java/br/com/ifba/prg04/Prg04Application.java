33f0964cc4599a0db0fa4a360b5fc85d22f3002e
atusoliverss <batistaatus131@gmail.com>
2025-02-05 22:34:05 -0300

    Que comecem os jogos

---
+++ prg04/src/main/java/br/com/ifba/prg04/Prg04Application.java
@@ -0,0 +1,13 @@
+package br.com.ifba.prg04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prg04Application.class, args);
+    }
+
+}
