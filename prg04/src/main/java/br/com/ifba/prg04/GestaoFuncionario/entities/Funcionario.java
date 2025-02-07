37c1623b75e4cdfdf0d59685429b9e6ced1a6d4f
RianSantos09 <riansantosn45@gmail.com>
2025-02-07 12:17:41 -0300

    adicionando table funcionário

--- prg04/src/main/java/br/com/ifba/prg04/GestaoFuncionario/entities/Funcionario.java
+++ prg04/src/main/java/br/com/ifba/prg04/GestaoFuncionario/entities/Funcionario.java
@@ -1,2 +1,44 @@
 package br.com.ifba.prg04.GestaoFuncionario.entities;
 
+import br.com.ifba.prg04.infrastructure.entity.PersistenceEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "funcionarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Funcionario extends PersistenceEntity {
+
+    @Column(nullable = false, unique = true)
+    private String codigo;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String telefone;
+
+}
