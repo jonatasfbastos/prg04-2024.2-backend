5e9bdb77d9de193939575399efef2efef6456e71
RianSantos09 <riansantosn45@gmail.com>
2025-02-07 12:37:37 -0300

    adicionando service a funcionario

--- prg04/src/main/java/br/com/ifba/prg04/GestaoFuncionario/services/FuncionarioService.java
+++ prg04/src/main/java/br/com/ifba/prg04/GestaoFuncionario/services/FuncionarioService.java
@@ -1,1 +1,36 @@
 package br.com.ifba.prg04.GestaoFuncionario.services;
+
+
+import br.com.ifba.prg04.GestaoFuncionario.entities.Funcionario;
+import br.com.ifba.prg04.GestaoFuncionario.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public Funcionario saveFuncionario(Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+
+    public List<Funcionario> getAllFuncionarios() {
+        return funcionarioRepository.findAll();
+    }
+
+    public Funcionario getFuncionarioById(Long id) {
+        return funcionarioRepository.findById(id).orElse(null);
+    }
+
+    public void deleteFuncionario(Long id) {
+        funcionarioRepository.deleteById(id);
+    }
+
+    public Funcionario updateFuncionario(Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+}
