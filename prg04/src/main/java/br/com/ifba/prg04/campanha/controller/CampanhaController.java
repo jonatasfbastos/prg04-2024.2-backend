47783b632565c4cfccaa4075abd3484045ce1395
PedroMBarreto <pedrobarretom019@gmail.com>
2025-02-08 09:41:25 -0300

    Campanha conectado ao banco de dados

--- prg04/src/main/java/br/com/ifba/prg04/campanha/controller/CampanhaController.java
+++ prg04/src/main/java/br/com/ifba/prg04/campanha/controller/CampanhaController.java
@@ -1,29 +1,26 @@
 package br.com.ifba.prg04.campanha.controller;
 
 import br.com.ifba.prg04.campanha.entity.Campanha;
+import br.com.ifba.prg04.campanha.repository.CampanhaRepository;
 import br.com.ifba.prg04.campanha.service.CampanhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequestMapping("/campanha")
-@Controller
+@RestController
 public class CampanhaController {
 
     @Autowired
-    private CampanhaService cs;
+    private CampanhaRepository campanhaR;
 
     @GetMapping("/findall")
-    public List<Campanha> findAll() {
-        return cs.findAll();
-    }
-
-    @PostMapping("/save")
-    public Campanha save(Campanha c) {
-        return cs.save(c);
+    public List<Campanha> findall() {
+        return (List<Campanha>) campanhaR.findAll();
     }
 }
--- prg04/src/main/java/br/com/ifba/prg04/campanha/entity/Campanha.java
+++ prg04/src/main/java/br/com/ifba/prg04/campanha/entity/Campanha.java
@@ -1,20 +1,84 @@
 package br.com.ifba.prg04.campanha.entity;
 
-import br.com.ifba.prg04.vacinacao.entities.Vacina;
 import jakarta.persistence.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(name="campanha")
 public class Campanha {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
-    private String nome;
+    @Column(name="nomec")
+    @NotNull
+    private String nomec;
+
+    @NotNull
+    @Column(name="vacina")
     private String vacina;
+
+    @NotNull
+    @Column(name="publico_alvo")
     private String publicoAlvo;
+
+    @NotNull
+    @Column(name="data_inicio")
     private String dataInicio;
+
+    @NotNull
+    @Column(name="data_fim")
     private String dataFim;
 
+    //Get e Set
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(String dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getPublicoAlvo() {
+        return publicoAlvo;
+    }
+
+    public void setPublicoAlvo(String publicoAlvo) {
+        this.publicoAlvo = publicoAlvo;
+    }
+
+    public String getVacina() {
+        return vacina;
+    }
+
+    public void setVacina(String vacina) {
+        this.vacina = vacina;
+    }
+
+    public String getNomec() {
+        return nomec;
+    }
+
+    public void setNomec(String nomec) {
+        this.nomec = nomec;
+    }
 }
