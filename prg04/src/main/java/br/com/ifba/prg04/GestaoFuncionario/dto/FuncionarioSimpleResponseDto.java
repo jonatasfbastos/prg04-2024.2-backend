c7481ce6a793483d62974bbeacdaeb576a59c961
RianSantos09 <riansantosn45@gmail.com>
2025-02-12 10:19:56 -0300

    Update FuncionarioSimpleResponseDto.java

--- prg04/src/main/java/br/com/ifba/prg04/GestaoFuncionario/dto/FuncionarioSimpleResponseDto.java
+++ prg04/src/main/java/br/com/ifba/prg04/GestaoFuncionario/dto/FuncionarioSimpleResponseDto.java
@@ -1,16 +1,5 @@
 package br.com.ifba.prg04.GestaoFuncionario.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class FuncionarioSimpleResponseDto {
-
-    private String codigo;
-
+    
 }
