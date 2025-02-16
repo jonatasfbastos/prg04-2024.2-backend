7e48c3e412ce7303159eb91e2e3681af9e422f95
PedroMBarreto <pedrobarretom019@gmail.com>
2025-02-16 10:56:05 -0300

    Endpoints
    Criado Post, Get, Put e delete para a classe campanha

--- prg04/src/main/java/br/com/ifba/prg04/campanha/controller/CampanhaController.java
+++ prg04/src/main/java/br/com/ifba/prg04/campanha/controller/CampanhaController.java
@@ -2,15 +2,11 @@ package br.com.ifba.prg04.campanha.controller;
 
 import br.com.ifba.prg04.campanha.entity.Campanha;
 import br.com.ifba.prg04.campanha.repository.CampanhaRepository;
-import br.com.ifba.prg04.campanha.service.CampanhaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/campanha")
 @RestController
@@ -19,8 +15,30 @@ public class CampanhaController {
     @Autowired
     private CampanhaRepository campanhaR;
 
+
+    @PostMapping("/save")
+    public Campanha save(@RequestBody Campanha campanha) {
+        return campanhaR.save(campanha);
+    }
+
     @GetMapping("/findall")
     public List<Campanha> findall() {
-        return (List<Campanha>) campanhaR.findAll();
+        return (List<Campanha>)campanhaR.findAll();
+    }
+
+    @PutMapping("/update")
+    public Campanha update(@RequestBody Campanha campanha) {
+        return campanhaR.save(campanha);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Optional<Campanha> delete (@PathVariable Long id) {
+        Optional <Campanha> campanha = campanhaR.findById(id);
+        if (campanha.isPresent()) {
+            campanhaR.deleteById(id);
+        } else {
+            return Optional.empty();
+        }
+        return campanha;
     }
 }
