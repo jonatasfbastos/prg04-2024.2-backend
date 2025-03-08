a5d0c750c14bcd829432c037fb73e0400d8aa73d
DevMartins <h.martins.silva2014@gmail.com>
2025-03-07 23:16:39 -0300

    Feat: Buscando pelo nome e Categoria dos Medicamentos

--- prg04/src/main/java/br/com/ifba/prg04/medicamento/service/IMedicamentoService.java
+++ prg04/src/main/java/br/com/ifba/prg04/medicamento/service/IMedicamentoService.java
@@ -5,11 +5,15 @@ import br.com.ifba.prg04.medicamento.entity.Medicamento;
 import org.springframework.data.domain.Page;
 
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface IMedicamentoService {
     Page<Medicamento> findAll(Pageable pageable);
     Optional<Medicamento> findById(Long id);
+    List<Medicamento> findByNome(String nome);
+    List<Medicamento> findByCategoria(String nome);
     Medicamento save(Medicamento medicamento);
     Medicamento update(Long id, Medicamento medicamento);
     void deleteById(Long id);
